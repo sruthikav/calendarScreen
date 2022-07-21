93c2367244a25b2ba6c7e7ae712b45d6ea225fec
Sruthi Kavuri <kav.sruthi@gmail.com>
2022-07-20 20:50:29 -0700

    iteration 0 complete

--- calendarScreen/ViewController.swift
+++ calendarScreen/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+import SwiftUI
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var workoutLabel: UILabel!
+    @IBOutlet weak var motivationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordWorkoutButton(_ sender: Any) {
+        motivationLabel.text = "great job! you're on your path to reach your fitness goals. make sure you hydrate yourself and eat well :)"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        let strDate = dateFormatter.string(from: datePicker.date)
+        workoutLabel.text = strDate
+        
+    }
+    
 }
 
---
+++ calendarScreen/WorkoutDate.swift
@@ -0,0 +1,12 @@
+//
+//  WorkoutDate.swift
+//  calendarScreen
+//
+//  Created by Sruthi Kavuri on 7/20/22.
+//
+
+import UIKit
+
+class WorkoutDate {
+    var date = ""
+}
